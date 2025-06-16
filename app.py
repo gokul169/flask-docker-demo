f4d9c514927f6e6ca0087d3aefd8f03e6ee66804
gokul169 <gokulsumithra2005@gmail.com>
2025-06-16 14:12:47 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+﻿from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Docker!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
